540945f5f82bc6b61529b5b4dda5146a3fe99071
Jonathan Tung <jtung23@gmail.com>
2018-11-08 21:40:07 -0800

    connected server and react

---
+++ server/index.js
@@ -0,0 +1,34 @@
+//dependencies
+const express = require('express');
+var bodyParser = require("body-parser");
+const path = require('path');
+const app = express();
+const routes = require("../routes");
+// var db = require("../models");
+//connections
+const PORT = process.env.PORT || 5000;
+
+// Priority serve any static files.
+app.use(express.static(path.resolve(__dirname, '../react-ui/public')));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(routes);
+
+// Answer API requests.
+app.get('/api', function (req, res) {
+  res.set('Content-Type', 'application/json');
+  res.send('{"message":"Hello from the custom server!"}');
+});
+
+
+// All remaining requests return the React app, so it can handle routing.
+app.get('*', function(request, response) {
+  response.sendFile(path.resolve(__dirname, '../react-ui/public', 'index.html'));
+});
+
+app.listen(PORT, function () {
+  console.log(`Listening on port ${PORT}`);
+});
+
+
+
